8a800eba02c2dcb2f92804d44e4c8027ed05306d
Hunter Morgan <automaticgiant@gmail.com>
2016-03-15 08:02:21 -0400

    fix script fail without archive

--- derby.sh
+++ derby.sh
@@ -12,7 +12,7 @@ cd $(dirname $0)
 echo "cd $(pwd)"
 archive_prefix="db-derby-10.12.1.1-lib"
 lib_url=http://www-us.apache.org/dist/db/derby/db-derby-10.12.1.1/db-derby-10.12.1.1-lib.zip
-ls "$archive_prefix".zip
+#ls "$archive_prefix".zip
 test -f "$archive_prefix".zip || curl -O $lib_url
 derbyrun_path="$archive_prefix/lib/derbyrun.jar"
 test -f $derbyrun_path || unzip "$archive_prefix".zip
