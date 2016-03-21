559e8e76f31d4f5d84d9865d2467bb3c516e3208
Hunter Morgan <automaticgiant@gmail.com>
2016-03-21 13:46:12 -0400

    separated init/load of derby handler
    * entry point (should be sourced) is `$repodir/load.zsh`
    * registers derby function which has a `--help`

---
+++ derby.zsh
@@ -0,0 +1,33 @@
+#!/usr/bin/env zsh
+set -eo pipefail
+
+cd $(dirname $0)
+local archive_prefix="db-derby-10.12.1.1-lib"
+local lib_url=http://www-us.apache.org/dist/db/derby/db-derby-10.12.1.1/db-derby-10.12.1.1-lib.zip
+#ls "$archive_prefix".zip
+
+test -f "$archive_prefix".zip || curl -O $lib_url
+local derby_run_path="$archive_prefix/lib/derbyrun.jar"
+
+test -f $derby_run_path || unzip "$archive_prefix".zip
+local derby_run="$(readlink -f $derby_run_path)"
+
+derby(){
+if test "$1" = "--help" ; then
+  echo Usage:
+  echo -e "\t$0 \t\tto download(if needed) and start"
+  echo -e "\t$0 stop \tto stop"
+  exit
+fi
+
+if test "$1" = "stop" ; then
+  echo trying to stop
+  java -jar $derby_run server ping && java -jar $derby_run server shutdown
+else
+  echo starting if not already
+  java -jar $derby_run server ping || java -jar $derby_run server start &
+fi
+}
+
+echo derby handler installed
+
