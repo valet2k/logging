9da35d576623327b7adea7c2d769951e67f722df
Hunter Morgan <automaticgiant@gmail.com>
2016-04-07 00:12:06 -0400

    update load script

--- load.zsh
+++ load.zsh
@@ -1,2 +1,15 @@
 #!/usr/bin/env zsh
+local dir=$(dirname $0)
+local olddir=$PWD
+
+cd $dir # logging directory
 source derby.zsh
+derby&
+
+export valet2k_ng=$PWD/nailgun/ng
+
+cd assistant
+pgrep -f com.github.valet2k || mvn exec:java&
+
+cd $olddir
+source addhook.zsh
