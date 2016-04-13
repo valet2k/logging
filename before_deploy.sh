511ae725b8c44479645a8e744a8fbf309c930b9e
Hunter Morgan <automaticgiant@gmail.com>
2016-04-13 18:49:34 -0400

    display sha for jar

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,7 +4,7 @@ s3=https://s3.amazonaws.com/valet2k/builds/
 mkdir deploy
 mv assistant/target/assistant*normalized.jar deploy/$jarname
 cd deploy
-sha512sum $jarname > $jarname.sha512
+sha512sum $jarname | tee $jarname.sha512
 curl $s3$jarname.sha512 | sha512sum -c && rm $jarname $jarname.sha512
 cd ..
 find deploy
