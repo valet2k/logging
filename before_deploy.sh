cb855a2b569d8dd29554f436ed291c1bdc3118e1
Hunter Morgan <automaticgiant@gmail.com>
2016-04-13 16:54:34 -0400

    fix checksumming with sha512

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 jarname=assistant-latest.jar
+s3=https://s3.amazonaws.com/valet2k/builds/
 mkdir deploy
 mv assistant/target/assistant*.jar deploy/$jarname
 cd deploy
-wget https://s3.amazonaws.com/valet2k/builds/$jarname.md5
-md5sum -c $jarname.md5 && rm $jarname && md5sum $jarname > $jarname.md5
+sha512sum $jarname > $jarname.sha512
+curl $s3$jarname.sha512 | sha512sum -c && rm $jarname $jarname.sha512
 cd ..
+find deploy
