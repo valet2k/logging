ac6044d5093de4e2381be3e1d6c042cf89d34168
Hunter Morgan <automaticgiant@gmail.com>
2016-04-13 16:42:27 -0400

    fix deploy

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 jarname=assistant-latest.jar
-mv assistant/target/assistant*.jar deploy/$jarname
 mkdir deploy
+mv assistant/target/assistant*.jar deploy/$jarname
 cd deploy
 wget https://s3.amazonaws.com/valet2k/builds/$jarname.md5
 md5sum -c $jarname.md5 && rm $jarname && md5sum $jarname > $jarname.md5
