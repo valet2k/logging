eb2a4b1a6622c61e29cf9dbbaca54b1908769357
Hunter Morgan <automaticgiant@gmail.com>
2016-04-13 18:33:22 -0400

    fix deploy jar name

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,7 +2,7 @@
 jarname=assistant-latest.jar
 s3=https://s3.amazonaws.com/valet2k/builds/
 mkdir deploy
-mv assistant/target/assistant*.jar deploy/$jarname
+mv assistant/target/assistant*normalized.jar deploy/$jarname
 cd deploy
 sha512sum $jarname > $jarname.sha512
 curl $s3$jarname.sha512 | sha512sum -c && rm $jarname $jarname.sha512
