2262a886ebfdd14067405941f3d349dd06ade7c1
Hunter Morgan <automaticgiant@gmail.com>
2016-04-13 16:31:24 -0400

    update scripts
    * quote location properly
    * remove extinct scripts
    * update gitignore

--- load.zsh
+++ load.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-valet2k_repo=$(dirname $(readlink -e $0))
+valet2k_repo=$(dirname "$(readlink -e "$0")")
 lastpwd=$PWD
 
 make -C $valet2k_repo/nailgun-git ng
@@ -10,12 +10,10 @@ autoload add-zsh-hook
 add-zsh-hook -d precmd log
 function log(){
   #get detailed env and send last history line as args
-  #TODO: change
   typeset | $valet2k_ng lognew $(fc -ln -1)
 }
 add-zsh-hook precmd log
 
 #TODO: startup
-#cd assistant
 #../nailgun/ng ng-alias | grep lognew > /dev/null || nohup mvn exec:java&
 
