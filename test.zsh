edd7f39562d815f2c6099806bb7186b21029f4aa
Hunter Morgan <automaticgiant@gmail.com>
2016-04-22 01:27:59 -0400

    fix broken e2e test
    * lognew needed a eof sent in to prevent hang
    * skip nohup on test

--- test.zsh
+++ test.zsh
@@ -4,11 +4,11 @@ setopt ERR_EXIT
 setopt PRINT_EXIT_VALUE
 
 source ./load.zsh
-(cd $valet2k_repo/assistant && nohup java -jar ./target/assistant-*-normalized.jar)&
-sleep 20
+(cd $valet2k_repo/assistant && java -jar ./target/assistant-*-normalized.jar&)
+sleep 10
 
 $valet2k_ng ng-alias | grep lognew
-$valet2k_ng lognew testentry
+echo | $valet2k_ng lognew testentry
 $valet2k_ng logshow | grep testentry
 $valet2k_ng logshow | grep logging
 
