ff206d329d9097e0e4c460d7cf656044b13bf6fb
max <maxwell3@vt.edu>
2016-04-25 05:00:50 -0400

    Add more widgets that call create-suggestions
    Add magic space, delete-char (forwards and back), tab completion still
    not working.

--- v2k-init/create-suggestions.zsh
+++ v2k-init/create-suggestions.zsh
@@ -5,9 +5,10 @@ function v2k-create-suggestions {
   echo "$(clear)Suggestion last updated: $(date +"%T")" > ~/.v2k_suggestion_pipe
 }
 
+#Widget not working, may make this its own function and bind tab to it
 function expand-or-complete {
   v2k-create-suggestions &!
-  zle .self-insert
+  zle .expand-or-complete
 }
 
 zle -N expand-or-complete
@@ -18,3 +19,24 @@ function self-insert {
 }
 
 zle -N self-insert
+
+function backward-delete-char {
+  v2k-create-suggestions &!
+  zle .backward-delete-char
+}
+
+zle -N backward-delete-char
+
+function delete-char {
+  v2k-create-suggestions &!
+  zle .delete-char
+}
+
+zle -N delete-char
+
+function magic-space {
+  v2k-create-suggestions &!
+  zle .magic-space
+}
+
+zle -N magic-space
