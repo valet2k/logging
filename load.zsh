4ffaf6a014a34c9c22aaa8e94cbb5eb34c1ff2e3
sfbahr <sfbahr@gmail.com>
2016-04-27 02:52:07 -0400

    Setup load.zsh for inclusion in .zshrc
    Only launch into valet2k_tmux if $valet2k_auto_tmux is set to true

--- load.zsh
+++ load.zsh
@@ -5,21 +5,27 @@ if [[ ! -p ~/.v2k_suggestion_pipe ]]; then
   mkfifo ~/.v2k_suggestion_pipe
 fi
 
-export valet2k_repo=$(dirname "$(readlink -e "$0")")
-make -C $valet2k_repo/nailgun-git ng
-export valet2k_ng=$valet2k_repo/nailgun-git/ng
-
 
 #TODO: startup
 #../nailgun/ng ng-alias | grep lognew > /dev/null || nohup mvn exec:java&
 
-socket_name=v2kcom
-# shorthand
-export tm="tmux -L $socket_name"
 
-echo "Entering Valet 2000"
-if ${=tm} info &> /dev/null; then #running
-  echo "jk, v2k already running"
-else #not running
-  ${valet2k_repo}/v2k-init/tmux-init.zsh
+if [[ "$valet2k_in_tmux" = true ]]; then
+  source ${valet2k_init}/suggestions.zsh
+  source ${valet2k_init}/logging.zsh
+  source ${valet2k_init}/create-suggestions.zsh
+else
+  valet2k_repo=$(dirname "$(readlink -e "$0")")
+  make -C $valet2k_repo/nailgun-git ng > /dev/null
+  export valet2k_init=$valet2k_repo/v2k-init
+  export valet2k_ng=$valet2k_repo/nailgun-git/ng
+  export valet2k_tmux="tmux -L v2kcom"
+  
+  if [[ "$valet2k_auto_tmux" = true ]]; then #set valet2k_auto_tmux in .zshrc
+    ${valet2k_init}/tmux-init.zsh
+  else
+    echo 'To start valet2k_tmux, execute "${valet2k_init}/tmux-init.zsh".'
+    echo 'For now only logging is enabled.'
+    source ${valet2k_init}/logging.zsh
+  fi
 fi
--- v2k-init/tmux-init.zsh
+++ v2k-init/tmux-init.zsh
@@ -1,12 +1,17 @@
 #!/usr/bin/env zsh
-echo "new-session"
-${=tm} new-session -d -s user #"zsh zsh_hook.sh)\'; zsh -i"
-#$tm new-window -d -t user -n user
-echo "split-window"
-${=tm} split-window -dl 11 -t user:0.0 "while true; do cat ~/.v2k_suggestion_pipe; done"
-echo "send-keys"
-${=tm} send-keys -t user:0.0 "source ${valet2k_repo}/v2k-init/suggestions.zsh;
-source ${valet2k_repo}/v2k-init/logging.zsh;
-source ${valet2k_repo}/v2k-init/create-suggestions.zsh" C-m
-echo "attach"
-${=tm} attach -t user
+
+if [[ $ZSH_EVAL_CONTEXT =~ :file$ ]]; then
+  echo 'You must execute tmux-init, not source it.'
+  return 1
+fi
+
+if ${=valet2k_tmux} info &> /dev/null; then #running
+  echo "v2k already running"
+else #not running
+  export valet2k_in_tmux=true
+  echo "Entering Valet 2000:"
+  ${=valet2k_tmux} new-session -d -s user #"zsh zsh_hook.sh)\'; zsh -i"
+  #$tm new-window -d -t user -n user
+  ${=valet2k_tmux} split-window -dl 11 -t user:0.0 "while true; do cat ~/.v2k_suggestion_pipe; done"
+  ${=valet2k_tmux} attach -t user
+fi
