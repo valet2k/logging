5d3dc4ea9999f223bc3f31c757ab93bc6686488f
Hunter Morgan <automaticgiant@gmail.com>
2016-04-27 12:41:46 -0400

    verbosify test.zsh

--- test.zsh
+++ test.zsh
@@ -3,12 +3,18 @@
 setopt ERR_EXIT
 setopt PRINT_EXIT_VALUE
 
+echo sourcing load.zsh
 source ./load.zsh
+echo starting assistant in background
 (cd $valet2k_repo/assistant && java -jar ./target/assistant-*-normalized.jar&)
+echo waiting for 10s for startup
 sleep 10
 
+echo testing lognew
 $valet2k_ng ng-alias | grep lognew
 echo | $valet2k_ng lognew testentry
+echo looking for command
 $valet2k_ng logshow | grep testentry
+echo looking for workingdir
 $valet2k_ng logshow | grep logging
 
