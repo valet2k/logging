a41fe3c88f2c81384587a6bf747004a3f2d19057
Hunter Morgan <automaticgiant@gmail.com>
2016-04-27 12:35:26 -0400

    move up logging and make so always done

--- load.zsh
+++ load.zsh
@@ -10,15 +10,15 @@ fi
 #../nailgun/ng ng-alias | grep lognew > /dev/null || nohup mvn exec:java&
 
 valet2k_repo=$(dirname "$(readlink -e "$0")")
+make -C $valet2k_repo/nailgun-git ng > /dev/null
 export valet2k_ng=$valet2k_repo/nailgun-git/ng
+source ${valet2k_init}/logging.zsh
 ${valet2k_ng} logml train
 
 if [[ "$valet2k_in_tmux" = true ]]; then
   source ${valet2k_init}/suggestions.zsh
-  source ${valet2k_init}/logging.zsh
   source ${valet2k_init}/create-suggestions.zsh
 else
-  make -C $valet2k_repo/nailgun-git ng > /dev/null
   export valet2k_init=$valet2k_repo/v2k-init
   export valet2k_tmux="tmux -L v2kcom"
 
@@ -27,6 +27,5 @@ else
   else
     echo 'To start valet2k_tmux, execute "${valet2k_init}/tmux-init.zsh".'
     echo 'For now only logging is enabled.'
-    source ${valet2k_init}/logging.zsh
   fi
 fi
