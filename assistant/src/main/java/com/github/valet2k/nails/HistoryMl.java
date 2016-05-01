50a36cfd4fc796f3ee116bbe19b5b983622b865f
Jayanth Prathipati <jayanth@vt.edu>
2016-05-01 17:39:43 -0400

    changed labelling process

--- assistant/src/main/java/com/github/valet2k/nails/HistoryMl.java
+++ assistant/src/main/java/com/github/valet2k/nails/HistoryMl.java
@@ -14,17 +14,25 @@ import jsat.text.HashedTextVectorCreator;
 import jsat.text.tokenizer.NaiveTokenizer;
 import jsat.text.wordweighting.WordCount;
 import jsat.utils.Pair;
+import jsat.utils.SystemInfo;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.output.StringBuilderWriter;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.DoubleAccumulator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Comparator.reverseOrder;
+import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.reverseOrder;
 
 /**
  * Created by automaticgiant on 4/6/16.
@@ -56,7 +64,7 @@ public class HistoryMl {
         RegressionDataSet regressionDataSet;
         switch (ctx.getArgs()[0]) {
             case "train":
-                train();
+                train(ctx);
                 ctx.exit(0);
             case "test":
                 connection = Core.pool.getConnection();
@@ -74,11 +82,11 @@ public class HistoryMl {
                 regressionDataSet = new RegressionDataSet(points.stream()
                         .parallel()
                         .map(Pair::getFirstItem)
-                        .collect(Collectors.toList()));
-                getModel().train(regressionDataSet);
+                        .collect(toList()));
+                getModel(ctx).train(regressionDataSet);
                 points.stream().parallel().map(p -> {
                     try {
-                        return p.getSecondItem() + getModel().regress(p.getFirstItem().getDataPoint());
+                        return p.getSecondItem() + getModel(ctx).regress(p.getFirstItem().getDataPoint());
                     } catch (SQLException e) {
                         return 0;
                     }
@@ -94,13 +102,12 @@ public class HistoryMl {
                     DataPoint dataPoint = new DataPoint(vec);
                     suggestions.add(new Pair<>(dataPoint, string));
                 }
-
                 List<String> collect = suggestions
                         .stream()
                         .parallel()
                         .map(p -> {
                             try {
-                                return new Pair<>(getModel().regress(p.getFirstItem()), p.getSecondItem());
+                                return new Pair<>(getModel(ctx).regress(p.getFirstItem()), p.getSecondItem());
                             } catch (SQLException e) {
                                 e.printStackTrace();
                                 return new Pair<>(0.0, p.getSecondItem());
@@ -109,7 +116,7 @@ public class HistoryMl {
                         .sorted(Comparator.comparing(Pair::getFirstItem))
                         .limit(10)
                         .map(Pair::getSecondItem)
-                        .collect(Collectors.toList());
+                        .collect(toList());
                 Integer number = null;
                 try {
                     number = Integer.valueOf(ctx.getArgs()[1]);
@@ -122,33 +129,39 @@ public class HistoryMl {
                 }
                 ctx.exit(0);
             case "predict":
-                ctx.out.println(getModel().regress(new DataPoint(HASHED_TEXT_VECTOR_CREATOR.newText(ctx.getArgs()[1]))));
+                ctx.out.println(getModel(ctx).regress(new DataPoint(HASHED_TEXT_VECTOR_CREATOR.newText(ctx.getArgs()[1]))));
                 ctx.exit(0);
         }
     }
 
-    public static RandomForest getModel() throws SQLException {
-        if (!trained) train();
+    public static RandomForest getModel(NGContext ctx) throws SQLException {
+        if (!trained) train(ctx);
         return RANDOM_FOREST;
     }
 
-    private static void train() throws SQLException {
+    private static void train(NGContext ctx) throws SQLException {
         Connection connection = Core.pool.getConnection();
         ResultSet resultSet = connection.createStatement().executeQuery("SELECT LASTCOMMAND,WORKINGDIRECTORY,TYPESET FROM VALET2K_HISTORY WHERE LASTCOMMAND IS NOT NULL AND WORKINGDIRECTORY IS NOT NULL AND TYPESET IS NOT NULL");
         List<DataPointPair<Double>> trainingPoints = new ArrayList<>();
+
+        HashMap<String, Integer> freqMap = frequencyFromList(resultSet);
+
+        HashMap<String, Integer> top3Map = top3Freq(freqMap);
+
         while (resultSet.next()) {
             String string = resultSet.getString(LASTCOMMAND);
             String typeset = resultSet.getString(TYPESET);
-            double label = labelFromTypeset(typeset);
+            String directory = resultSet.getString(WORKINGDIRECTORY);
+            double label = labelFromTypeset(typeset) + labelFromDirectory(directory, ctx.getWorkingDirectory()) + labelFromExetension(ctx.getWorkingDirectory(), string) + labelFromFrequency(top3Map, string);
             Vec vec = HASHED_TEXT_VECTOR_CREATOR.newText(string);
             DataPoint dataPoint = new DataPoint(vec);
             trainingPoints.add(new DataPointPair<>(dataPoint, label));
         }
         RegressionDataSet regressionDataSet = new RegressionDataSet(trainingPoints.stream()
                 .parallel()
-                .collect(Collectors.toList()));
+                .collect(toList()));
         RANDOM_FOREST.train(regressionDataSet);
-        trained=true;
+        trained = true;
     }
 
     public static String pipestatusFromTypeset(String s) {
@@ -170,6 +183,108 @@ public class HistoryMl {
     public static Double labelFromPipestatus(String s) {
         if (s == null || s.isEmpty()) return 0.0; // support for empty/null - throw out for training
         String[] statuses = s.split(" ");
-        return Stream.of(statuses).map(Integer::valueOf).anyMatch(integer -> !integer.equals(0)) ? -50.0 : 10.0;
+        return Stream.of(statuses).map(Integer::valueOf).anyMatch(integer -> !integer.equals(0)) ? 0.0 : -0.0;
     }
+
+    public static Double labelFromDirectory(String s, String currentDirectory) {
+        Double counter = 0.0;
+        String[] items = s.split("/");
+        String[] currentDir = currentDirectory.split("/");
+        if (items.length > 4 && currentDir.length > 4) {
+            if (items[3].compareTo(currentDir[3]) == 0) {
+                counter += 1.0;
+            }
+            if (items[4].compareTo(currentDir[4]) == 0) {
+                counter += 1.0;
+            }
+        }
+        return counter;
+    }
+
+
+    public static Double labelFromExetension(String currentDirectory, String command) {
+        File f = new File(currentDirectory);
+        File[] listOfFiles = f.listFiles();
+        String[] splited = command.split(" ");
+        Boolean gitFlag = false;
+        Boolean makeFlag = false;
+        Boolean mvnFlag = false;
+
+        Double counter = 0.0;
+        System.out.println("COMMAND NAME: " + splited[0]);
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+
+                if(file.getName().contains("git")) {
+                    gitFlag = true;
+                }
+                if(file.getName().contains("make")) {
+                    makeFlag = true;
+                }
+                else if(file.getName().contains("pom.xml")) {
+                    mvnFlag = true;
+                }
+            }
+        }
+
+        if(command.equals("git") && gitFlag) {
+            counter += 1;
+        }
+
+        if(command.equals("make") && makeFlag) {
+            counter += 1;
+        }
+
+        if(command.equals("mvn") && mvnFlag) {
+            counter += 1;
+        }
+        return counter;
+    }
+
+    public static HashMap<String, Integer> top3Freq(HashMap<String, Integer> resultMap) {
+        HashMap<String, Integer> hm  = new HashMap<String, Integer>();
+
+
+        List<Integer> list = new ArrayList<Integer>(resultMap.values());
+        Collections.sort(list, Collections.reverseOrder());
+        List<Integer> top3 = list.subList(0, 3);
+
+        for(Integer i : top3) {
+            Iterator<Map.Entry<String, Integer>> iter = resultMap.entrySet().iterator();
+            while (iter.hasNext()) {
+                Map.Entry<String, Integer> entry = iter.next();
+                if (entry.getValue().equals(i)) {
+                    String key = entry.getKey();
+                    hm.put(key, i);
+                }
+            }
+        }
+        return hm;
+
+
+
+    }
+    public static HashMap<String, Integer> frequencyFromList(ResultSet resultSet) throws SQLException {
+
+        HashMap<String, Integer> hm  = new HashMap<String, Integer>();
+
+        while (resultSet.next()) {
+            String string = resultSet.getString(LASTCOMMAND);
+            String[] splited = string.split(" ");
+
+            if(hm.containsKey(splited[0])) {
+                hm.put(splited[0], hm.get(splited[0]) + 1);
+            }
+            else {
+                hm.put(splited[0], 1);
+            }
+        }
+        return hm;
+    }
+
+    public static Double labelFromFrequency(HashMap<String, Integer> top3Freq, String command) {
+        return top3Freq.containsKey(command.split(" ")[0]) ? 50.0 : 0.0;
+    }
+
 }
