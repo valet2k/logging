f2725817ecd74d654bfc9fcf726038c3dd68b647
Hunter Morgan <automaticgiant@gmail.com>
2016-05-03 10:56:06 -0400

    add simple benchmark/observational script

---
+++ assistant/bmark.zsh
@@ -0,0 +1,47 @@
+#!/usr/bin/env zsh
+$valet2k_ng ng-stop
+(mvn exec:java &> assistant.log) &
+sleep 10
+
+prefix=$(git rev-parse HEAD)
+
+date > $prefix.default.log
+$valet2k_ng logml train &>> $prefix.default.log
+date >> $prefix.default.log
+$valet2k_ng logml suggest &>> $prefix.default.log
+date >> $prefix.default.log
+$valet2k_ng logml $prefix &>> $prefix.default.log
+date >> $prefix.default.log
+
+date > $prefix.second.log
+$valet2k_ng logml train &>> $prefix.second.log
+date >> $prefix.second.log
+$valet2k_ng logml suggest &>> $prefix.second.log
+date >> $prefix.second.log
+$valet2k_ng logml $prefix &>> $prefix.second.log
+date >> $prefix.second.log
+
+
+date > $prefix.10000.log
+$valet2k_ng logml train -v 10000 &>> $prefix.10000.log
+date >> $prefix.10000.log
+$valet2k_ng logml suggest &>> $prefix.10000.log
+date >> $prefix.10000.log
+$valet2k_ng logml $prefix &>> $prefix.10000.log
+date >> $prefix.10000.log
+
+date > $prefix.50000.log
+$valet2k_ng logml train -v 50000 &>> $prefix.50000.log
+date >> $prefix.50000.log
+$valet2k_ng logml suggest &>> $prefix.50000.log
+date >> $prefix.50000.log
+$valet2k_ng logml $prefix &>> $prefix.50000.log
+date >> $prefix.50000.log
+
+date > $prefix.50000.log
+$valet2k_ng logml train -v 50000 &>> $prefix.50000.log
+date >> $prefix.50000.log
+$valet2k_ng logml suggest &>> $prefix.50000.log
+date >> $prefix.50000.log
+$valet2k_ng logml $prefix &>> $prefix.50000.log
+date >> $prefix.50000.log
